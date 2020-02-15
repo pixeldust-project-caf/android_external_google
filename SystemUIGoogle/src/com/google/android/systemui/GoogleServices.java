ezio84/abc_external_google

Restore custom app actions

SystemUIGoogle/src/com/google/android/systemui/GoogleServices.java
@@ -31,37 +31,37 @@ public class GoogleServices extends VendorServices {
     }
 
     // Small broadcast receiver for helping with Application actions
-    // private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
-    //     @Override
-    //     public void onReceive(Context context, Intent intent) {
-    //         String action = intent.getAction();
-    //         if(action.equals(Intent.ACTION_PACKAGE_REMOVED)) {
-    //             // Get packageName from Uri
-    //             String packageName = intent.getData().getSchemeSpecificPart();
-    //             // If the package is still installed
-    //             if (Utils.isPackageInstalled(context, packageName)) {
-    //                 // it's an application update, we can skip the rest.
-    //                 return;
-    //             }
-    //             // Get package names currently set as default
-    //             String shortPackageName = Settings.Secure.getStringForUser(context.getContentResolver(),
-    //                     Settings.Secure.SHORT_SQUEEZE_CUSTOM_APP,
-    //                     UserHandle.USER_CURRENT);
-    //             String longPackageName = Settings.Secure.getStringForUser(context.getContentResolver(),
-    //                     Settings.Secure.LONG_SQUEEZE_CUSTOM_APP,
-    //                     UserHandle.USER_CURRENT);
-    //             // if the package name equals to some set value
-    //             if(packageName.equals(shortPackageName)) {
-    //                 // The short application action has to be reset
-    //                 resetApplicationAction(/* isShortAction */ true);
-    //             }
-    //             if (packageName.equals(longPackageName)) {
-    //                 // The long application action has to be reset
-    //                 resetApplicationAction(/* isShortAction */ false);
-    //             }
-    //         }
-    //     }
-    // };
+    private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(action.equals(Intent.ACTION_PACKAGE_REMOVED)) {
+                // Get packageName from Uri
+                String packageName = intent.getData().getSchemeSpecificPart();
+                // If the package is still installed
+                if (Utils.isPackageInstalled(context, packageName)) {
+                    // it's an application update, we can skip the rest.
+                    return;
+                }
+                // Get package names currently set as default
+                String shortPackageName = Settings.Secure.getStringForUser(context.getContentResolver(),
+                        Settings.Secure.SHORT_SQUEEZE_CUSTOM_APP,
+                        UserHandle.USER_CURRENT);
+                String longPackageName = Settings.Secure.getStringForUser(context.getContentResolver(),
+                        Settings.Secure.LONG_SQUEEZE_CUSTOM_APP,
+                        UserHandle.USER_CURRENT);
+                // if the package name equals to some set value
+                if(packageName.equals(shortPackageName)) {
+                    // The short application action has to be reset
+                    resetApplicationAction(/* isShortAction */ true);
+                }
+                if (packageName.equals(longPackageName)) {
+                    // The long application action has to be reset
+                    resetApplicationAction(/* isShortAction */ false);
+                }
+            }
+        }
+    };
 
     public void start() {
         StatusBar statusBar = (StatusBar) SysUiServiceProvider.getComponent(mContext, StatusBar.class);
@@ -70,11 +70,11 @@ public class GoogleServices extends VendorServices {
         }
 
         // Intent for applications that get uninstalled
-        // IntentFilter filter = new IntentFilter();
-        // filter.addAction(Intent.ACTION_PACKAGE_REMOVED);
-        // filter.addDataScheme("package");
-        // // Register our BroadcastReceiver
-        // mContext.registerReceiver(mBroadcastReceiver, filter);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_PACKAGE_REMOVED);
+        filter.addDataScheme("package");
+        // Register our BroadcastReceiver
+        mContext.registerReceiver(mBroadcastReceiver, filter);
     }
 
     /* Helper that sets to default values, this gets called when a package
@@ -85,17 +85,17 @@ public class GoogleServices extends VendorServices {
             Settings.Secure.putIntForUser(mContext.getContentResolver(),
                     Settings.Secure.SHORT_SQUEEZE_SELECTION, /* no action */ 0,
                     UserHandle.USER_CURRENT);
-            // Settings.Secure.putStringForUser(mContext.getContentResolver(),
-            //         Settings.Secure.SHORT_SQUEEZE_CUSTOM_APP_FR_NAME, /* none */ "",
-            //         UserHandle.USER_CURRENT);
+            Settings.Secure.putStringForUser(mContext.getContentResolver(),
+                    Settings.Secure.SHORT_SQUEEZE_CUSTOM_APP_FR_NAME, /* none */ "",
+                    UserHandle.USER_CURRENT);
         } else {
             // Remove stored values
             Settings.Secure.putIntForUser(mContext.getContentResolver(),
                     Settings.Secure.LONG_SQUEEZE_SELECTION, /* no action */ 0,
                     UserHandle.USER_CURRENT);
-            // Settings.Secure.putStringForUser(mContext.getContentResolver(),
-            //         Settings.Secure.LONG_SQUEEZE_CUSTOM_APP_FR_NAME, /* none */ "",
-            //         UserHandle.USER_CURRENT);
+            Settings.Secure.putStringForUser(mContext.getContentResolver(),
+                    Settings.Secure.LONG_SQUEEZE_CUSTOM_APP_FR_NAME, /* none */ "",
+                    UserHandle.USER_CURRENT);
         }
     }
 }

SystemUIGoogle/src/com/google/android/systemui/elmyra/actions/ShortSqueezeCustomApp.java
@@ -1,57 +1,58 @@
-// package com.google.android.systemui.elmyra.actions;
+package com.google.android.systemui.elmyra.actions;
 
-// import android.content.pm.ActivityInfo;
-// import android.os.UserHandle;
-// import android.provider.Settings;
-// import android.view.View;
-// import android.widget.ListView;
+import android.content.pm.ActivityInfo;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ListView;
 
-// import com.dirtyunicorns.support.preferences.AppPicker;
+import com.abc.support.R;
+import com.abc.support.preferences.AppPicker;
 
-// public class ShortSqueezeCustomApp extends AppPicker {
+public class ShortSqueezeCustomApp extends AppPicker {
 
-//     @Override
-//     protected void onListItemClick(ListView l, View v, int position, long id) {
-//         if (!mIsActivitiesList) {
-//             // we are in the Apps list
-//             String packageName = applist.get(position).packageName;
-//             String friendlyAppString = (String) applist.get(position).loadLabel(packageManager);
-//             setPackage(packageName, friendlyAppString);
-//             setPackageActivity(null);
-//         } else if (mIsActivitiesList) {
-//             // we are in the Activities list
-//             setPackageActivity(mActivitiesList.get(position));
-//         }
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        if (!mIsActivitiesList) {
+            // we are in the Apps list
+            String packageName = applist.get(position).packageName;
+            String friendlyAppString = (String) applist.get(position).loadLabel(packageManager);
+            setPackage(packageName, friendlyAppString);
+            setPackageActivity(null);
+        } else if (mIsActivitiesList) {
+            // we are in the Activities list
+            setPackageActivity(mActivitiesList.get(position));
+        }
 
-//         mIsActivitiesList = false;
-//         finish();
-//     }
+        mIsActivitiesList = false;
+        finish();
+    }
 
-//     @Override
-//     protected void onLongClick(int position) {
-//         if (mIsActivitiesList) return;
-//         String packageName = applist.get(position).packageName;
-//         String friendlyAppString = (String) applist.get(position).loadLabel(packageManager);
-//         // always set xxx_SQUEEZE_CUSTOM_APP so we can fallback if something goes wrong with
-//         // packageManager.getPackageInfo
-//         setPackage(packageName, friendlyAppString);
-//         setPackageActivity(null);
-//         showActivitiesDialog(packageName);
-//     }
+    @Override
+    protected void onLongClick(int position) {
+        if (mIsActivitiesList) return;
+        String packageName = applist.get(position).packageName;
+        String friendlyAppString = (String) applist.get(position).loadLabel(packageManager);
+        // always set xxx_SQUEEZE_CUSTOM_APP so we can fallback if something goes wrong with
+        // packageManager.getPackageInfo
+        setPackage(packageName, friendlyAppString);
+        setPackageActivity(null);
+        showActivitiesDialog(packageName);
+    }
 
-//     protected void setPackage(String packageName, String friendlyAppString) {
-//         Settings.Secure.putStringForUser(getContentResolver(),
-//                 Settings.Secure.SHORT_SQUEEZE_CUSTOM_APP, packageName,
-//                 UserHandle.USER_CURRENT);
-//         Settings.Secure.putStringForUser(getContentResolver(),
-//                 Settings.Secure.SHORT_SQUEEZE_CUSTOM_APP_FR_NAME, friendlyAppString,
-//                 UserHandle.USER_CURRENT);
-//     }
+    protected void setPackage(String packageName, String friendlyAppString) {
+        Settings.Secure.putStringForUser(getContentResolver(),
+                Settings.Secure.SHORT_SQUEEZE_CUSTOM_APP, packageName,
+                UserHandle.USER_CURRENT);
+        Settings.Secure.putStringForUser(getContentResolver(),
+                Settings.Secure.SHORT_SQUEEZE_CUSTOM_APP_FR_NAME, friendlyAppString,
+                UserHandle.USER_CURRENT);
+    }
 
-//     protected void setPackageActivity(ActivityInfo ai) {
-//         Settings.Secure.putStringForUser(
-//                 getContentResolver(), Settings.Secure.SHORT_SQUEEZE_CUSTOM_ACTIVITY,
-//                 ai != null ? ai.name : "NONE",
-//                 UserHandle.USER_CURRENT);
-//     }
-// }
+    protected void setPackageActivity(ActivityInfo ai) {
+        Settings.Secure.putStringForUser(
+                getContentResolver(), Settings.Secure.SHORT_SQUEEZE_CUSTOM_ACTIVITY,
+                ai != null ? ai.name : "NONE",
+                UserHandle.USER_CURRENT);
+    }
+}

pixeldust-project-caf/android_external_google

Signed-off-by: spezi77 <spezi7713@gmx.net>

SystemUIGoogle/src/com/google/android/systemui/GoogleServices.java
@@ -31,37 +31,37 @@ public class GoogleServices extends VendorServices {
     }
 
     // Small broadcast receiver for helping with Application actions
-    // private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
-    //     @Override
-    //     public void onReceive(Context context, Intent intent) {
-    //         String action = intent.getAction();
-    //         if(action.equals(Intent.ACTION_PACKAGE_REMOVED)) {
-    //             // Get packageName from Uri
-    //             String packageName = intent.getData().getSchemeSpecificPart();
-    //             // If the package is still installed
-    //             if (PixeldustUtils.isPackageInstalled(context, packageName)) {
-    //                 // it's an application update, we can skip the rest.
-    //                 return;
-    //             }
-    //             // Get package names currently set as default
-    //             String shortPackageName = Settings.Secure.getStringForUser(context.getContentResolver(),
-    //                     Settings.Secure.SHORT_SQUEEZE_CUSTOM_APP,
-    //                     UserHandle.USER_CURRENT);
-    //             String longPackageName = Settings.Secure.getStringForUser(context.getContentResolver(),
-    //                     Settings.Secure.LONG_SQUEEZE_CUSTOM_APP,
-    //                     UserHandle.USER_CURRENT);
-    //             // if the package name equals to some set value
-    //             if(packageName.equals(shortPackageName)) {
-    //                 // The short application action has to be reset
-    //                 resetApplicationAction(/* isShortAction */ true);
-    //             }
-    //             if (packageName.equals(longPackageName)) {
-    //                 // The long application action has to be reset
-    //                 resetApplicationAction(/* isShortAction */ false);
-    //             }
-    //         }
-    //     }
-    // };
+    private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(action.equals(Intent.ACTION_PACKAGE_REMOVED)) {
+                // Get packageName from Uri
+                String packageName = intent.getData().getSchemeSpecificPart();
+                // If the package is still installed
+                if (PixeldustUtils.isPackageInstalled(context, packageName)) {
+                    // it's an application update, we can skip the rest.
+                    return;
+                }
+                // Get package names currently set as default
+                String shortPackageName = Settings.Secure.getStringForUser(context.getContentResolver(),
+                        Settings.Secure.SHORT_SQUEEZE_CUSTOM_APP,
+                        UserHandle.USER_CURRENT);
+                String longPackageName = Settings.Secure.getStringForUser(context.getContentResolver(),
+                        Settings.Secure.LONG_SQUEEZE_CUSTOM_APP,
+                        UserHandle.USER_CURRENT);
+                // if the package name equals to some set value
+                if(packageName.equals(shortPackageName)) {
+                    // The short application action has to be reset
+                    resetApplicationAction(/* isShortAction */ true);
+                }
+                if (packageName.equals(longPackageName)) {
+                    // The long application action has to be reset
+                    resetApplicationAction(/* isShortAction */ false);
+                }
+            }
+        }
+    };
 
     public void start() {
         StatusBar statusBar = (StatusBar) SysUiServiceProvider.getComponent(mContext, StatusBar.class);
@@ -70,11 +70,11 @@ public class GoogleServices extends VendorServices {
         }
 
         // Intent for applications that get uninstalled
-        // IntentFilter filter = new IntentFilter();
-        // filter.addAction(Intent.ACTION_PACKAGE_REMOVED);
-        // filter.addDataScheme("package");
-        // // Register our BroadcastReceiver
-        // mContext.registerReceiver(mBroadcastReceiver, filter);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_PACKAGE_REMOVED);
+        filter.addDataScheme("package");
+        // Register our BroadcastReceiver
+        mContext.registerReceiver(mBroadcastReceiver, filter);
     }
 
     /* Helper that sets to default values, this gets called when a package
@@ -85,17 +85,17 @@ public class GoogleServices extends VendorServices {
             Settings.Secure.putIntForUser(mContext.getContentResolver(),
                     Settings.Secure.SHORT_SQUEEZE_SELECTION, /* no action */ 0,
                     UserHandle.USER_CURRENT);
-            // Settings.Secure.putStringForUser(mContext.getContentResolver(),
-            //         Settings.Secure.SHORT_SQUEEZE_CUSTOM_APP_FR_NAME, /* none */ "",
-            //         UserHandle.USER_CURRENT);
+            Settings.Secure.putStringForUser(mContext.getContentResolver(),
+                    Settings.Secure.SHORT_SQUEEZE_CUSTOM_APP_FR_NAME, /* none */ "",
+                    UserHandle.USER_CURRENT);
         } else {
             // Remove stored values
             Settings.Secure.putIntForUser(mContext.getContentResolver(),
                     Settings.Secure.LONG_SQUEEZE_SELECTION, /* no action */ 0,
                     UserHandle.USER_CURRENT);
-            // Settings.Secure.putStringForUser(mContext.getContentResolver(),
-            //         Settings.Secure.LONG_SQUEEZE_CUSTOM_APP_FR_NAME, /* none */ "",
-            //         UserHandle.USER_CURRENT);
+            Settings.Secure.putStringForUser(mContext.getContentResolver(),
+                    Settings.Secure.LONG_SQUEEZE_CUSTOM_APP_FR_NAME, /* none */ "",
+                    UserHandle.USER_CURRENT);
         }
     }
 }

SystemUIGoogle/src/com/google/android/systemui/elmyra/actions/ShortSqueezeCustomApp.java
@@ -1,57 +1,58 @@
-// package com.google.android.systemui.elmyra.actions;
+package com.google.android.systemui.elmyra.actions;
 
-// import android.content.pm.ActivityInfo;
-// import android.os.UserHandle;
-// import android.provider.Settings;
-// import android.view.View;
-// import android.widget.ListView;
+import android.content.pm.ActivityInfo;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ListView;
 
-// import com.dirtyunicorns.support.preferences.AppPicker;
+import com.pixeldust.support.R;
+import com.pixeldust.support.preferences.AppPicker;
 
-// public class ShortSqueezeCustomApp extends AppPicker {
+public class ShortSqueezeCustomApp extends AppPicker {
 
-//     @Override
-//     protected void onListItemClick(ListView l, View v, int position, long id) {
-//         if (!mIsActivitiesList) {
-//             // we are in the Apps list
-//             String packageName = applist.get(position).packageName;
-//             String friendlyAppString = (String) applist.get(position).loadLabel(packageManager);
-//             setPackage(packageName, friendlyAppString);
-//             setPackageActivity(null);
-//         } else if (mIsActivitiesList) {
-//             // we are in the Activities list
-//             setPackageActivity(mActivitiesList.get(position));
-//         }
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        if (!mIsActivitiesList) {
+            // we are in the Apps list
+            String packageName = applist.get(position).packageName;
+            String friendlyAppString = (String) applist.get(position).loadLabel(packageManager);
+            setPackage(packageName, friendlyAppString);
+            setPackageActivity(null);
+        } else if (mIsActivitiesList) {
+            // we are in the Activities list
+            setPackageActivity(mActivitiesList.get(position));
+        }
 
-//         mIsActivitiesList = false;
-//         finish();
-//     }
+        mIsActivitiesList = false;
+        finish();
+    }
 
-//     @Override
-//     protected void onLongClick(int position) {
-//         if (mIsActivitiesList) return;
-//         String packageName = applist.get(position).packageName;
-//         String friendlyAppString = (String) applist.get(position).loadLabel(packageManager);
-//         // always set xxx_SQUEEZE_CUSTOM_APP so we can fallback if something goes wrong with
-//         // packageManager.getPackageInfo
-//         setPackage(packageName, friendlyAppString);
-//         setPackageActivity(null);
-//         showActivitiesDialog(packageName);
-//     }
+    @Override
+    protected void onLongClick(int position) {
+        if (mIsActivitiesList) return;
+        String packageName = applist.get(position).packageName;
+        String friendlyAppString = (String) applist.get(position).loadLabel(packageManager);
+        // always set xxx_SQUEEZE_CUSTOM_APP so we can fallback if something goes wrong with
+        // packageManager.getPackageInfo
+        setPackage(packageName, friendlyAppString);
+        setPackageActivity(null);
+        showActivitiesDialog(packageName);
+    }
 
-//     protected void setPackage(String packageName, String friendlyAppString) {
-//         Settings.Secure.putStringForUser(getContentResolver(),
-//                 Settings.Secure.SHORT_SQUEEZE_CUSTOM_APP, packageName,
-//                 UserHandle.USER_CURRENT);
-//         Settings.Secure.putStringForUser(getContentResolver(),
-//                 Settings.Secure.SHORT_SQUEEZE_CUSTOM_APP_FR_NAME, friendlyAppString,
-//                 UserHandle.USER_CURRENT);
-//     }
+    protected void setPackage(String packageName, String friendlyAppString) {
+        Settings.Secure.putStringForUser(getContentResolver(),
+                Settings.Secure.SHORT_SQUEEZE_CUSTOM_APP, packageName,
+                UserHandle.USER_CURRENT);
+        Settings.Secure.putStringForUser(getContentResolver(),
+                Settings.Secure.SHORT_SQUEEZE_CUSTOM_APP_FR_NAME, friendlyAppString,
+                UserHandle.USER_CURRENT);
+    }
 
-//     protected void setPackageActivity(ActivityInfo ai) {
-//         Settings.Secure.putStringForUser(
-//                 getContentResolver(), Settings.Secure.SHORT_SQUEEZE_CUSTOM_ACTIVITY,
-//                 ai != null ? ai.name : "NONE",
-//                 UserHandle.USER_CURRENT);
-//     }
-// }
+    protected void setPackageActivity(ActivityInfo ai) {
+        Settings.Secure.putStringForUser(
+                getContentResolver(), Settings.Secure.SHORT_SQUEEZE_CUSTOM_ACTIVITY,
+                ai != null ? ai.name : "NONE",
+                UserHandle.USER_CURRENT);
+    }
+}