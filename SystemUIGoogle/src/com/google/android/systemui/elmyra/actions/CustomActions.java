ezio84/abc_external_google

Restore all custom squeeze actions

SystemUIGoogle/src/com/google/android/systemui/elmyra/actions/CustomActions.java
@@ -83,19 +83,19 @@ public class CustomActions extends Action {
                 break;
             case 9: // Notification panel
                 if (isScreenOn) {
-                    // Utils.toggleNotifications();
+                    Utils.toggleNotifications();
                 }
                 break;
             case 10: // QS panel
                 if (isScreenOn) {
-                    // Utils.toggleQsPanel();
+                    Utils.toggleQsPanel();
                 }
                 break;
             case 11: // Clear notifications
-                // Utils.clearAllNotifications();
+                Utils.clearAllNotifications();
                 break;
             case 12: // Ringer modes
-                // Utils.toggleRingerModes(getContext());
+                Utils.toggleRingerModes(getContext());
                 break;
         }
     }

pixeldust-project-caf/android_external_google

Signed-off-by: spezi77 <spezi7713@gmx.net>

SystemUIGoogle/src/com/google/android/systemui/elmyra/actions/CustomActions.java
@@ -88,14 +88,14 @@ public class CustomActions extends Action {
                 break;
             case 10: // QS panel
                 if (isScreenOn) {
-                    // PixeldustUtils.toggleQsPanel();
+                    PixeldustUtils.toggleQsPanel();
                 }
                 break;
             case 11: // Clear notifications
-                // PixeldustUtils.clearAllNotifications();
+                PixeldustUtils.clearAllNotifications();
                 break;
             case 12: // Ringer modes
-                // PixeldustUtils.toggleRingerModes(getContext());
+                PixeldustUtils.toggleRingerModes(getContext());
                 break;
         }
     }